4a0dbd441a62bff69166c5727541b60e58f8842a
Harrison Ervin <pikmaster5business@gmail.com>
2021-08-23 16:15:38 +1000

    Add files via upload

---
+++ sounds.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.mixer.pre_init()
+
+class Sounds: 
+    def __init__(self):
+        self.button_sound = pygame.mixer.Sound("assets/sounds/flap_wing.wav")
+        self.death_sound = pygame.mixer.Sound("assets/sounds/flap_die.wav")
+        self.point_sound = pygame.mixer.Sound("assets/sounds/flap_point.wav")
+        self.point_sound_countdown = 1000
+
+    def play_flap_sound(self):
+        self.button_sound.play()
+
+    def play_collision_sound(self):
+        self.death_sound.play()
+
+    def play_score_sound(self):
+        self.point_sound.play()
+        self.point_sound_countdown = 1000
